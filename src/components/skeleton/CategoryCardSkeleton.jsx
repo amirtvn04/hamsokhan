a022f71c92fd34c5f06890c90e5773ab0586b1be
amirtavana <amirtvn80@gmail.com>
2026-02-14 01:45:22 +0330

    feat(skelet): add categories skeleton

---
+++ src/components/skeleton/CategoryCardSkeleton.jsx
@@ -0,0 +1,18 @@
+import Skeleton from "./Skeleton"
+
+export default function CategoryCardSkeleton() {
+    return (
+        <div className="bg-white border border-black/15 rounded-lg px-6 py-6">
+            <Skeleton className="size-12.5 inline-block" />
+
+            <Skeleton className="h-5 mt-3 w-28" />
+            <Skeleton className="h-5 mt-3" />
+            <Skeleton className="h-5 mt-1 w-48 " />
+
+            <div className="flex items-center justify-between mt-5">
+                <Skeleton className="h-5 w-15" />
+                <Skeleton className="h-5 w-15" />
+            </div>
+        </div>
+    )
+}
--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,23 +1,13 @@
 import CategoryCard from "../components/CategoryCard"
 import InputField from "../components/elements/InputField"
+import CategoryCardSkeleton from "../components/skeleton/CategoryCardSkeleton";
+import StatCardSkeleton from "../components/skeleton/StatCardSkeleton";
 import { useCategories } from "../features/forums/useCategories"
 import { useGlobalStats } from "../features/forums/useGlobalStats"
 
 function Categories() {
   const { categories, Categoryloading } = useCategories();
   const { global, Globalloading } = useGlobalStats();
-
-  if (Categoryloading) {
-    return (
-      <div className="mt-22">صبر کنید</div>
-    )
-  }
-
-  if (Globalloading) {
-    return (
-      <div className="mt-22">صبرggg کنید</div>
-    )
-  }
 
   return (
     <main className='pt-15.5 container-uni'>
@@ -26,31 +16,47 @@ function Categories() {
         <p className="text-gray-500 font-semibold mt-3 mb-8">دسته‌بندی مورد نظر خود را انتخاب کنید تا انجمن‌های مرتبط را مشاهده کنید</p>
         <InputField icon='search' type='text' CustomStyles='w-full pr-12 pl-5 py-3 bg-white' placeholder='جستجو در بین دسته‌بندی‌ها...' />
       </div>
-      {global.map(g => (
-        <div key={'1'} className="grid grid-cols-4 gap-3.5 mt-8 mb-8">
-          <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
-            <span className="font-bold text-2xl">{g.categories_count.toLocaleString('fa-IR')}</span>
-            <span className="text-sm text-gray-500">دسته بندی</span>
-          </div>
-          <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
-            <span className="font-bold text-2xl">{g.forums_count.toLocaleString('fa-IR')}</span>
-            <span className="text-sm text-gray-500">انجمن</span>
-          </div>
-          <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
-            <span className="font-bold text-2xl">{g.topics_count.toLocaleString('fa-IR')}</span>
-            <span className="text-sm text-gray-500">تاپیک</span>
-          </div>
-          <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
-            <span className="font-bold text-2xl">{g.posts_count.toLocaleString('fa-IR')}</span>
-            <span className="text-sm text-gray-500">پست</span>
-          </div>
-        </div>
-      ))}
+
+      <div className="grid grid-cols-4 gap-3.5 mt-8 mb-8">
+        {Globalloading ?
+          Array.from({ length: 4 }).map((_, i) => (
+            <StatCardSkeleton key={i} />
+          ))
+          :
+          global.length > 0 && (
+            <>
+              <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
+                <span className="font-bold text-2xl">{global[0]?.categories_count.toLocaleString('fa-IR')}</span>
+                <span className="text-sm text-gray-500">دسته بندی</span>
+              </div>
+              <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
+                <span className="font-bold text-2xl">{global[0]?.forums_count.toLocaleString('fa-IR')}</span>
+                <span className="text-sm text-gray-500">انجمن</span>
+              </div>
+              <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
+                <span className="font-bold text-2xl">{global[0]?.topics_count.toLocaleString('fa-IR')}</span>
+                <span className="text-sm text-gray-500">تاپیک</span>
+              </div>
+              <div className="bg-white rounded-lg p-6 flex flex-col items-center justify-center border border-black/15 hover:border-[#0065F4] transition duration-150">
+                <span className="font-bold text-2xl">{global[0]?.posts_count.toLocaleString('fa-IR')}</span>
+                <span className="text-sm text-gray-500">پست</span>
+              </div>
+            </>
+          )
+        }
+      </div>
 
       <section className="grid grid-cols-4 gap-3.5">
-        {categories.map(cat => (
-          <CategoryCard key={cat.id} {...cat} />
-        ))}
+        {Categoryloading ?
+          Array.from({ length: 8 }).map((_, i) => (
+            <CategoryCardSkeleton key={i} />
+          ))
+          :
+          categories.map(cat => (
+            <CategoryCard key={cat.id} {...cat} />
+          ))
+        }
+
       </section>
     </main>
   )
